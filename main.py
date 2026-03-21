16d61dc51fd51ec9a427b97a5c83a45e99c9bd44
giddeladewoye20-droid <giddeladewoye20@gmail.com>
2026-03-21 18:02:38 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -11,12 +11,29 @@ screen=pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("PAQUETA CRISPS")
 run=True
 
-bg1=pygame.image.load(r"rocketinspace\rocket.png")
+
+bg1=pygame.image.load(r"spritesinpygame\rocket.png")
 bg2=pygame.image.load(r"rocketinspace\background.png")
 
+class Rocket(pygame.sprite.Sprite):
+    def __init__(self,image,x,y):
+        super().__init__()
+        self.image=image
+        self.rect=self.image.get_rect()
+        self.rect.x=x
+        self.rect.y=y
+
+rocket=Rocket(bg1,300,300)
+spritegroup=pygame.sprite.Group()
+spritegroup.add(rocket)
+
+       
+
+        
 
 while run == True:
     screen.blit(bg2,(0,0))
+    spritegroup.draw(screen)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False 
